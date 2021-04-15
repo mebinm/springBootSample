d2f8181e5f3404532792f6e2c6107587af80e915
Mebin Mathews <mebinmathews@gmail.com.com>
2021-04-15 11:13:27 +0530

    first commit

---
+++ src/main/java/com/example/apm/demoApm/DemoApmApplication.java
@@ -0,0 +1,13 @@
+package com.example.apm.demoApm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apm/demoApm/DemoApmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apm.demoApm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
